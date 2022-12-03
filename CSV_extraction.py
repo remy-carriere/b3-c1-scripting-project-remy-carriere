4b2a6ec11eb97b5652d5a67a7c2c0d423c417ce6
remy.carriere <remy.carriere@etu.umontpellier.fr>
2022-12-03 21:44:43 +0100

    Cleaning & commentaries

--- CSV_extraction.py
+++ CSV_extraction.py
@@ -1,59 +1,76 @@
+"""
+Script Python permettant de récupérer un fichier nommé conso-annuelles_v1.csv,
+ayant pour colonnes ["Appareil suivi","ID logement","Consommation annuelle AN1", "Consommation annuelle AN2", "Type"]
+enlever toutes les lignes contenant une case vide,
+supprimer la colonne ID logement,
+additionner les colonnes "Consommation annuelle" entre elles,
+et rendre le résultat dans un fichier nommé conso-clean.csv
+"""
+
 import re
 import pandas as pd
 import csv
 
+'''
+J'ai fait le choix de ne pas utiliser de fonctions car ce script
+se base sur des boucles imbriquées dans d'autres boucles,
+et faire appel à des fonctions prend du temps sur l'exécution.
+Je considère que ce n'était pas nécessaire pour un script de cette taille.
+'''
+
 df = pd.read_csv('conso-annuelles_v1.csv', sep=';', encoding='latin-1')
 
-# print(df.columns)
-# print("Nombres de lignes avant cleaning :" + str(len(df.index)))
-
+# Permet d'enlever les lignes avec une case vide
 df_no_void = df.dropna()
-# print("Nombres de lignes après cleaning :" + str(len(df_no_void.index)))
 
+# Dictionnaire permettant d'initialiser un Dataframe
+# qui correspond aux lignes non-vides, et après vérification
+# des formats de consommation annuelle.
 data_added = {
     df.columns[0]: [],
     "Consommation annuelle": [],
     df.columns[4]: []
 }
 
+# Correspond aux lignes brutes du Dataframe, ce qui nous permet de les compter
 index = df_no_void.index
 
 for i in range(1, len(index)):
-    # VERIFICATION FORMAT DONNEES
-    # Si des cellules dans les colonnes 2 et 3 (Consommation) sont vides, on ne les utilise pas
-    if (str(df_no_void.loc[index[i]][2]) not in (None, "") and
-            str(df_no_void.loc[index[i]][3]) not in (None, "")):
-        # Autrement, si ces cellules ne contiennent pas uniquement des caractères numériques et une virgule,
-        # on le change en un 0 pour ne pas fausser les calculs, sinon on les cast en float en remplacant la virgule
-        # par un point
-        if re.search("([0-9+,])\w+", str(df_no_void.loc[index[i]][2])) is None:
-            first_el = 0
-        else:
-            if isinstance(df_no_void.loc[index[i]][2], str):
-                first_el = float(str.replace(df_no_void.loc[index[i]][2], ",", "."))
-            else:
-                first_el = df_no_void.loc[index[i]][2]
-        if re.search("([0-9+,])\w+", str(df_no_void.loc[index[i]][3])) is None:
-            second_el = 0
-        else:
-            if isinstance(df_no_void.loc[index[i]][3], str):
-                second_el = float(str.replace(df_no_void.loc[index[i]][3], ",", "."))
-            else:
-                second_el = df_no_void.loc[index[i]][3]
 
-        # SET VALEURS
-        # On ajoute les 2 consommations ensemble et on le rentre dans la case correspondante
-        data_added[df_no_void.columns[0]].append(df_no_void.loc[index[i]][0])
-        conso = round(first_el + second_el, 1)
-        data_added["Consommation annuelle"].append(conso)
-        data_added[df_no_void.columns[4]].append(df_no_void.loc[index[i]][4])
+    # ----------------------------- VERIFICATION FORMAT DONNEES------------------------------
+    # Si les cellules ne contiennent pas uniquement des caractères numériques et une virgule,
+    # on le change en un 0 pour ne pas fausser les calculs, sinon on les cast en float en remplacant la virgule
+    # par un point pour que Python puisse caster correctement
+    if re.search("([0-9+,])\w+", str(df_no_void.loc[index[i]][2])) is None:
+        first_el = 0
+    else:
+        if isinstance(df_no_void.loc[index[i]][2], str):
+            first_el = float(str.replace(df_no_void.loc[index[i]][2], ",", "."))
+        else:
+            first_el = df_no_void.loc[index[i]][2]
+    if re.search("([0-9+,])\w+", str(df_no_void.loc[index[i]][3])) is None:
+        second_el = 0
+    else:
+        if isinstance(df_no_void.loc[index[i]][3], str):
+            second_el = float(str.replace(df_no_void.loc[index[i]][3], ",", "."))
+        else:
+            second_el = df_no_void.loc[index[i]][3]
 
+    # -------------------------------- SET VALEURS---------------------------------------
+    # On ajoute les 2 consommations ensemble et on le rentre dans la case correspondante
+    data_added[df_no_void.columns[0]].append(df_no_void.loc[index[i]][0])
+    conso = round(first_el + second_el, 1)
+    data_added["Consommation annuelle"].append(conso)
+    data_added[df_no_void.columns[4]].append(df_no_void.loc[index[i]][4])
+
+# On traduit ce dictionnaire en un Dataframe
 df_added = pd.DataFrame(data_added)
 
+# Que l'on rentre dans un fichier CSV
 df_added.to_csv('intermediaire.csv', sep=';', encoding='latin-1')
 
 # On utilise un fichier intermédiaire CSV pour faciliter le tri avec des listes
-# plutôt qu'avec des DataFrames (selon les consignes).
+# plutôt qu'avec des DataFrames (selon les consignes, tri interdit via pandas).
 with open("intermediaire.csv", 'r', encoding="latin-1") as inter_input:
     inter_list = []
     r = csv.reader(inter_input, delimiter=";")
